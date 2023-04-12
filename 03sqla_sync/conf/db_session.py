b15e4fa953599eedd9fb2d6e8ffb97f0b0be5b63
Douglas Amaral <douglaxz@hotmail.com>
2023-04-12 16:19:21 -0300

    correções no engine

--- 03sqla_sync/conf/db_session.py
+++ 03sqla_sync/conf/db_session.py
@@ -1,33 +1,29 @@
 import sqlalchemy as sa
-
 from sqlalchemy.orm import sessionmaker
-
 from pathlib import Path
 from typing import Optional
-
 from sqlalchemy.orm import Session
 from sqlalchemy.future.engine import Engine
-
 from models.model_base import ModelBase
 
-__engine: Optional(Engine) = None
+__engine: Optional[Engine] = None
 
-def create_engine(sqllite:bool = False) -> Engine:
+def create_engine(sqlite: bool = False) -> Engine:
     global __engine
 
     if __engine:
-        return
-    
-    if sqllite:
+        raise ValueError("Engine already exists")
+
+    if sqlite:
         arquivo_db = 'db/picoles.sqlite'
         folder = Path(arquivo_db).parent
         folder.mkdir(parents=True, exist_ok=True)
 
         conn_str = f'sqlite:///{arquivo_db}'
-        __engine = sa.create_engine(url=conn_str, echo=False, connect_args={"chek_same_thread":False})
+        __engine = sa.create_engine(url=conn_str, echo=False, connect_args={"check_same_thread": False})
     
     else:
-        conn_str= "postgreesql://geek:university@localhost:5432/picoles"
+        conn_str = "postgresql://geek:university@localhost:5432/picoles"
         __engine = sa.create_engine(url=conn_str, echo=False)
 
     return __engine
@@ -36,8 +32,8 @@ def create_session() -> Session:
     global __engine
 
     if not __engine:
-        #create_engine() # para postgree
-        create_engine(sqlite=True) # para sqllite
+        # create_engine() # para postgree
+        create_engine(sqlite=True) # para sqlite
     
     __session = sessionmaker(__engine, expire_on_commit=False, class_=Session)
 
@@ -45,13 +41,8 @@ def create_session() -> Session:
 
     return session
 
-def create_table() -> None:
-    global __engine
-
-    if not __engine:
-        create_engine(sqlite=True) # para sqllite
+def create_tables() -> None:
+    engine = create_engine(sqlite=True)
     
     import models.__all_models
-    ModelBase.metadata.drop_all(__engine)
-    ModelBase.metadata.create_all(__engine)
-
+    ModelBase.metadata.create_all(engine)
